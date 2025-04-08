26f044f25c4f8233d9cd7e2e471cdcd4a5a5ade4
boukadi99 <mohameddhia.boukadi@esprit.tn>
2025-04-08 09:00:30 +0100

    Gateway

---
+++ src/main/java/pi/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package pi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
